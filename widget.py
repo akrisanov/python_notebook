eeb81f576e9e444be477fd8d0d300afaf0869e26
Andrey Krisanov <develop4net@gmail.com>
2019-03-10 16:49:12 +0300

    Simple example of unittest usage

---
+++ widget.py
@@ -0,0 +1,7 @@
+class Widget:
+    def __init__(self, title, size=(50, 50)):
+        self.title = title
+        self._size = size
+    
+    def size(self):
+        return self._size
---
+++ widget_test.py
@@ -0,0 +1,10 @@
+import unittest
+from widget import Widget
+
+class WidgetTest(unittest.TestCase):
+    def test_initial_size(self):
+        widget = Widget('My Widget')
+        self.assertEqual(widget.size(), (50, 50), 'Initial size is not (50, 50)')
+        
+if __name__ == '__main__':
+    unittest.main()
